ed4e140f0b0faadc4b4c18aeb09b0e982c54c21e
unknown <stephanielakin13@outlook.com>
2022-07-05 13:13:28 -0400

    End of  Angular setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SkiNet';
+  products: IProduct[];
+
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.http.get('https://localhost:7086/api/products?pagesize=50').subscribe(
+      (response: IPagination) => {
+        this.products = response.data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
